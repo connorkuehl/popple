06eb207d4f4b6d39f4c2ad68c82cd478ab75a4cd
Connor Kuehl <cipkuehl@gmail.com>
2021-06-20 09:08:54 -0500

    Document config.go

--- config.go
+++ config.go
@@ -2,6 +2,11 @@ package main
 
 import "github.com/jinzhu/gorm"
 
+// Config represents a server-associated Popple config.
+//
+// For example, if someone in Server A tells Popple to stop
+// announcing karma events, then it won't affect Popple's
+// announce state in Server B.
 type Config struct {
 	gorm.Model
 	GuildID    string
