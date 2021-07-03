977893154ec16277a3f48e56338af7a585c220be
Connor Kuehl <cipkuehl@gmail.com>
2021-07-03 07:10:16 -0700

    Add unit tests for ModKarma

--- command_test.go
+++ command_test.go
@@ -57,6 +57,72 @@ func TestCheckKarma(t *testing.T) {
 	}
 }
 
+func TestModKarma(t *testing.T) {
+	interactiveCases := []struct {
+		name    string
+		input   request
+		needles []string
+	}{
+		{"basic increment", request{message: "Test++"}, []string{hasKarma("Test", 1)}},
+		{"basic decrement", request{message: "Test--"}, []string{hasKarma("Test", -1)}},
+		{"many operations", request{message: "NoKarma SomeKarma++ LessKarma-- NoMoreKarma"}, []string{hasKarma("SomeKarma", 1), hasKarma("LessKarma", -1)}},
+	}
+
+	for _, tt := range interactiveCases {
+		t.Run(tt.name, func(t *testing.T) {
+			db, cleanup := makeScratchDB(t, []Entity{})
+			defer cleanup()
+
+			var rsp responseSink
+			ModKarma(tt.input, &rsp, db)
+			assertNumResponses(t, rsp, 1)
+			assertHasAllSubstrings(t, rsp.responses[0].value, tt.needles)
+		})
+	}
+
+	ignoreCases := []struct {
+		name  string
+		input request
+	}{
+		{"ignore in direct message context", request{isDM: true, message: "Test++"}},
+		{"ignore net zero operations", request{message: "Test++ Test--"}},
+	}
+
+	for _, tt := range ignoreCases {
+		t.Run(tt.name, func(t *testing.T) {
+			var rsp responseSink
+			ModKarma(tt.input, &rsp, nil)
+			assertNumResponses(t, rsp, 0)
+		})
+	}
+
+	dataCases := []struct {
+		name   string
+		input  request
+		before []Entity
+		after  []Entity
+	}{
+		{"reducing to zero removes row", request{message: "Test--"}, []Entity{{Name: "Test", Karma: 1}}, []Entity{}},
+		{"adjusting karma is saved to existing row", request{message: "Test++"}, []Entity{{Name: "Test", Karma: 1}}, []Entity{{Name: "Test", Karma: 2}}},
+		{"the first increment adds a new row", request{message: "Test++"}, []Entity{}, []Entity{{Name: "Test", Karma: 1}}},
+	}
+
+	for _, tt := range dataCases {
+		t.Run(tt.name, func(t *testing.T) {
+			db, cleanup := makeScratchDB(t, tt.before)
+			defer cleanup()
+
+			var rsp responseSink
+			ModKarma(tt.input, &rsp, db)
+
+			var actual []Entity
+			db.Find(&actual)
+
+			assertDataChanged(t, actual, tt.after)
+		})
+	}
+}
+
 func assertNumResponses(t *testing.T, rsp responseSink, expected int) {
 	if len(rsp.responses) != expected {
 		t.Errorf("expected %d responses, got %d %#v", expected, len(rsp.responses), rsp)
@@ -67,6 +133,27 @@ func assertHasAllSubstrings(t *testing.T, haystack string, needles []string) {
 	for _, needle := range needles {
 		if !strings.Contains(haystack, needle) {
 			t.Errorf("didn't find %q in %#v", needle, haystack)
+		}
+	}
+}
+
+func assertDataChanged(t *testing.T, actual, expected []Entity) {
+	if len(actual) != len(expected) {
+		t.Errorf("number of actual results different from expected: actual = %#v expected = %#v", actual, expected)
+	}
+
+	var expectMap = make(map[string]Entity)
+	for _, a := range actual {
+		expectMap[a.Name] = a
+	}
+
+	for _, e := range expected {
+		row, ok := expectMap[e.Name]
+		if !ok {
+			t.Errorf("didn't find expected row %#v in %#v", e, expected)
+		}
+		if row.Karma != e.Karma {
+			t.Errorf("wrong karma value for %#v - got %d, want %d", e, row.Karma, e.Karma)
 		}
 	}
 }
