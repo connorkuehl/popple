connorkuehl/popple

commit cb80090af8fbc54bd75134d1fe8693c35ad647d4

Add a command router
Replace the command dispatch table with a slightly more convenient-to-use-and-
test router abstraction.

There is a special route name, "*", which is a terminating catch-all route.

popple.go
@@ -3,12 +3,10 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"os/signal"
-	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
@@ -72,56 +70,38 @@ func main() {
 	}
 
 	cancel := make(chan struct{})
-	workQueue := make(chan commandFn, *numJobs)
+	workQueue := make(chan func(), *numJobs)
 
 	for i := uint(0); i < *numWorkers; i++ {
 		go worker(workQueue, cancel)
 	}
 
-	// Generate the command dispatch table up front.
-	//
-	// These are all closures to capture the state from this function,
-	// such as the GORM database (db).
-	//
-	// This is useful because the workQueue can simply be a workqueue
-	// of func() and we can supply all the relevent data items as needed
-	// from the closure capture.
-	cmds := []struct {
-		verb    string
-		hasArgs bool
-		command func(request, responseWriter) commandFn
-	}{
-		{"announce", true, func(req request, rsp responseWriter) commandFn {
-			return func() {
-				SetAnnounce(req, rsp, db)
-			}
-		}},
-		{"help", false, func(req request, rsp responseWriter) commandFn {
-			return func() {
-				SendHelp(req, rsp)
-			}
-		}},
-		{"karma", true, func(req request, rsp responseWriter) commandFn {
-			return func() {
-				CheckKarma(req, rsp, db)
-			}
-		}},
-		{"bot", false, func(req request, rsp responseWriter) commandFn {
-			return func() {
-				Bot(req, rsp, db)
-			}
-		}},
-		{"top", false, func(req request, rsp responseWriter) commandFn {
-			return func() {
-				Top(req, rsp, db)
-			}
-		}},
-		{"version", false, func(req request, rsp responseWriter) commandFn {
-			return func() {
-				SendVersion(req, rsp)
-			}
-		}},
-	}
+	router := router{}
+	router.bot = "@" + session.State.User.Username
+	router.addRoute("announce", func(req request, rsp responseWriter) {
+		SetAnnounce(req, rsp, db)
+	})
+	router.addRoute("help", func(req request, rsp responseWriter) {
+		SendHelp(req, rsp)
+	})
+	router.addRoute("karma", func(req request, rsp responseWriter) {
+		CheckKarma(req, rsp, db)
+	})
+	router.addRoute("bot", func(req request, rsp responseWriter) {
+		Bot(req, rsp, db)
+	})
+	router.addRoute("top", func(req request, rsp responseWriter) {
+		Top(req, rsp, db)
+	})
+	router.addRoute("version", func(req request, rsp responseWriter) {
+		SendVersion(req, rsp)
+	})
+
+	// just check for karma operations by default if no other commands
+	// were matched
+	router.addRoute("*", func(req request, rsp responseWriter) {
+		ModKarma(req, rsp, db)
+	})
 
 	session.AddHandler(func(s *discordgo.Session, m *discordgo.MessageCreate) {
 		// don't process messages sent by the bot
@@ -130,44 +110,12 @@ func main() {
 		}
 
 		isDM := len(m.GuildID) == 0
-		bot := "@" + s.State.User.Username
-
 		msg := m.ContentWithMentionsReplaced()
-		for _, c := range cmds {
-			var spacer string
-			if c.hasArgs {
-				spacer = " "
-			}
 
-			var strip string
-			fullPrefix := fmt.Sprintf("%s %s%s", bot, c.verb, spacer)
-			dmPrefix := fmt.Sprintf("%s%s", c.verb, spacer)
-			if strings.HasPrefix(msg, fullPrefix) {
-				strip = fullPrefix
-			} else if isDM && strings.HasPrefix(msg, dmPrefix) {
-				strip = dmPrefix
-			} else {
-				// message doesn't appear to be addressing the bot with a command
-				// just move on
-				continue
-			}
-
-			// remove the @Bot (if it's there) as well as the command text so
-			// the command processing layer doesn't need to worry about it
-			msg = msg[len(strip):]
-
-			req := request{isDM: len(m.Message.GuildID) == 0, guildID: m.Message.GuildID, message: msg}
-			rsp := response{s, m.Message}
-
-			workQueue <- c.command(req, rsp)
-			return
-		}
-
-		// default action is to just check for karma operations
+		req := request{isDM: isDM, guildID: m.Message.GuildID, message: msg}
+		rsp := response{s, m.Message}
 		workQueue <- func() {
-			req := request{isDM: len(m.Message.GuildID) == 0, guildID: m.Message.GuildID, message: msg}
-			rsp := response{s, m.Message}
-			ModKarma(req, rsp, db)
+			router.route(req, rsp)
 		}
 	})
 
@@ -190,7 +138,7 @@ func main() {
 	session.Close()
 }
 
-func worker(workQueue <-chan commandFn, cancel <-chan struct{}) {
+func worker(workQueue <-chan func(), cancel <-chan struct{}) {
 	for {
 		select {
 		case <-cancel:

command.go
@@ -17,6 +17,50 @@ import (
 	"gorm.io/gorm"
 )
 
+type router struct {
+	bot      string
+	routes   []route
+	catchall route
+}
+
+func (r *router) addRoute(name string, work commandFn) {
+	route := route{name, work}
+	if name == "*" {
+		r.catchall = route
+	} else {
+		r.routes = append(r.routes, route)
+	}
+}
+
+func (r *router) route(req request, rsp responseWriter) {
+	for _, route := range r.routes {
+		var stripPrefix string
+		fullPrefix := fmt.Sprintf("%s %s", r.bot, route.match)
+		dmPrefix := route.match
+
+		if strings.HasPrefix(req.message, fullPrefix) {
+			stripPrefix = fullPrefix
+		} else if req.isDM && strings.HasPrefix(req.message, dmPrefix) {
+			stripPrefix = dmPrefix
+		} else {
+			continue
+		}
+
+		req.message = strings.TrimSpace(req.message[len(stripPrefix):])
+		route.cmd(req, rsp)
+		return
+	}
+
+	if r.catchall.cmd != nil {
+		r.catchall.cmd(req, rsp)
+	}
+}
+
+type route struct {
+	match string
+	cmd   commandFn
+}
+
 type request struct {
 	isDM    bool
 	guildID string
@@ -49,7 +93,7 @@ func (r response) React(emojiID string) error {
 	return err
 }
 
-type commandFn func()
+type commandFn func(req request, rsp responseWriter)
 
 // CheckKarma allows server inhabitants to query karma levels
 // for subjects they have incremented or decremented over time.

command_test.go
@@ -340,6 +340,72 @@ func TestBot(t *testing.T) {
 
 }
 
+func TestRouter(t *testing.T) {
+	const bot string = "@Popple"
+
+	cases := []struct {
+		name   string
+		req    request
+		routes []route
+	}{
+		{"no routes", request{message: "asdf"}, []route{}},
+		{"catchall", request{}, []route{
+			{"help", func(req request, rsp responseWriter) {
+				t.Errorf("expected to be routed to catchall, but wasn't")
+			}},
+			{"*", func(req request, rsp responseWriter) {
+				// yay
+			}},
+		}},
+		{"username and command is stripped", request{message: bot + " help pass"}, []route{
+			{"help", func(req request, rsp responseWriter) {
+				if req.message != "pass" {
+					t.Errorf("got %s, want %s", req.message, "pass")
+				}
+			}},
+			{"*", func(req request, rsp responseWriter) {
+				t.Errorf("fell into catchall, should have been routed elsewhere")
+			}},
+		}},
+		{"username is required outside of DMs", request{message: "help"}, []route{
+			{"help", func(req request, rsp responseWriter) {
+				t.Errorf("made it to subcommand but bot wasn't mentioned")
+			}},
+			{"*", func(req request, rsp responseWriter) {
+				// yay
+			}},
+		}},
+		{"username is optional in DMs", request{message: "help", isDM: true}, []route{
+			{"help", func(req request, rsp responseWriter) {
+				// yay
+			}},
+			{"*", func(req request, rsp responseWriter) {
+				t.Errorf("fell into catchall, should have been routed elsewhere")
+			}},
+		}},
+		{"can use username in DMs if preferred", request{message: bot + " help", isDM: true}, []route{
+			{"help", func(req request, rsp responseWriter) {
+				// yay
+			}},
+			{"*", func(req request, rsp responseWriter) {
+				t.Errorf("fell into catchall, should have been routed elsewhere")
+			}},
+		}},
+	}
+
+	for _, tt := range cases {
+		t.Run(tt.name, func(t *testing.T) {
+			r := router{}
+			r.bot = bot
+			for _, route := range tt.routes {
+				r.addRoute(route.match, route.cmd)
+			}
+
+			r.route(tt.req, nil)
+		})
+	}
+}
+
 func entityToLeaderboard(entities []Entity) string {
 	builder := strings.Builder{}
 	for _, e := range entities {

commit e05ce555db36d0421810f98c8ca551180aceeace

Fix seg fault due to accessing data prior to Session.Open
The bot's name is not hard-coded, and so it will check the Discord
session to learn its username for when people are addressing it.

Fixes: cb80090 (Add a command router, 2021-07-04)

popple.go
@@ -69,6 +69,12 @@ func main() {
 		log.Fatalf("Failed to initialize Discord library: %s\n", err)
 	}
 
+	err = session.Open()
+	if err != nil {
+		log.Fatalf("Error connecting to Discord: %s\n", err)
+	}
+	log.Printf("Popple is online, running version %s\n", Version)
+
 	cancel := make(chan struct{})
 	workQueue := make(chan func(), *numJobs)
 
@@ -118,16 +124,6 @@ func main() {
 			router.route(req, rsp)
 		}
 	})
-
-	err = session.Open()
-	if err != nil {
-		/* Should these be `defer`red? */
-		close(cancel)
-		close(workQueue)
-		log.Fatalf("Error connecting to Discord: %s\n", err)
-	}
-
-	log.Printf("Popple is online, running version %s\n", Version)
 
 	sessionChannel := make(chan os.Signal, 1)
 	signal.Notify(sessionChannel, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)