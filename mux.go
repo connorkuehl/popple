66ed5f83ec58b33b81d9ed0ceb6874a9be9f248e
Connor Kuehl <cipkuehl@gmail.com>
2022-03-28 18:13:54 -0500

    document mux.go

--- mux.go
+++ mux.go
@@ -4,6 +4,8 @@ import (
 	"regexp"
 )
 
+// Mux routes a message based on its message content to one of the bot's
+// handlers.
 type Mux struct {
 	botName       string
 	handlers      map[*regexp.Regexp]interface{}
@@ -14,6 +16,9 @@ type Mux struct {
 	onLoserboard  LoserboardHandler
 }
 
+// NewMux constructs a Mux with the bot's addressable name (the name is
+// significant because it is expected that the message will be prefixed
+// with the bot's name if it is a command.)
 func NewMux(name string) *Mux {
 	m := &Mux{
 		botName:       name,
@@ -42,6 +47,11 @@ func NewMux(name string) *Mux {
 	return m
 }
 
+// Route takes a message and returns a handler type based on its content (if
+// the message is routed to a specific bot command, the command will be stripped
+// from the message in the returned 'body', leaving only the arguments to the
+// command in the 'body'; else, the original message is returned with the default
+//handler type.)
 func (m *Mux) Route(message string) (action interface{}, body string) {
 	for matcher, action := range m.handlers {
 		if matched := matcher.ReplaceAllString(message, ""); matched != message {
