89695b115a42ec27fded4a69accc175f3a869e84
Connor Kuehl <cipkuehl@gmail.com>
2022-03-28 18:13:25 -0500

    document handler.go

--- handler.go
+++ handler.go
@@ -10,8 +10,12 @@ import (
 	"github.com/connorkuehl/popple/parse"
 )
 
+// AnnounceHandler is the type returned by the Mux to denote that the action
+// being taken should be forwarded to Announce.
 type AnnounceHandler func(repo Repository, serverID string, on bool) error
 
+// ParseAnnounceArgs parses the arguments that must be forwarded to the
+// AnnounceHandler.
 func ParseAnnounceArgs(message string) (on bool, err error) {
 	scanner := bufio.NewScanner(strings.NewReader(message))
 	scanner.Split(bufio.ScanWords)
@@ -36,15 +40,22 @@ func ParseAnnounceArgs(message string) (on bool, err error) {
 	return on, nil
 }
 
+// BumpKarmaHandler is the type returned by the Mux to denote that the action
+// being taken should be forwarded to BumpKarma.
 type BumpKarmaHandler func(repo Repository, serverID string, increments map[string]int64) (levels map[string]int64, err error)
 
+// ParseBumpKarmaArgs
 func ParseBumpKarmaArgs(message string) (increments map[string]int64, err error) {
 	increments = parse.Subjects(message)
 	return increments, nil
 }
 
+// KarmaHandler is the type returned by the Mux to denote that the action being
+// taken should be forwarded to Karma.
 type KarmaHandler func(repo get.EntityRepository, serverID string, who map[string]struct{}) (levels map[string]int64, err error)
 
+// ParseKarmaArgs parses the arguments that must be forwarded to the
+// KarmaHandler.
 func ParseKarmaArgs(message string) (who map[string]struct{}, err error) {
 	increments := parse.Subjects(message)
 	if len(increments) < 1 {
@@ -57,18 +68,30 @@ func ParseKarmaArgs(message string) (who map[string]struct{}, err error) {
 	return who, nil
 }
 
+// LeaderboardHandler is the type returned by the Mux to denote that the action being
+// taken should be forwarded to Leaderboard.
 type LeaderboardHandler func(repo get.EntityRepository, serverID string, limit uint) ([]get.Entity, error)
 
+// ParseLeaderboardArgs parses the arguments that must be forwarded to the
+// LeaderboardHandler.
 func ParseLeaderboardArgs(message string) (limit uint, err error) {
 	return ParseBoardArgs(message)
 }
 
+// LoserboardHandler is the type returned by the Mux to denote that the action being
+// taken should be forwarded to Loserboard.
 type LoserboardHandler func(repo get.EntityRepository, serverID string, limit uint) ([]get.Entity, error)
 
+// ParseLoserboardArgs parses the arguments that must be forwarded to the
+// LoserboardHandler.
 func ParseLoserboardArgs(message string) (limit uint, err error) {
 	return ParseBoardArgs(message)
 }
 
+// ParseBoardArgs parses the arguments that must be forwarded to either
+// LeaderboardHandler or LoserboardHandler.
+//
+// TODO: this can be made private
 func ParseBoardArgs(message string) (limit uint, err error) {
 	scanner := bufio.NewScanner(strings.NewReader(message))
 	scanner.Split(bufio.ScanWords)
