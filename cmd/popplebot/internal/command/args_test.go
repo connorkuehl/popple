aa2336668dd454b2b0abd6cb01d64dc45da10d82
Connor Kuehl <cipkuehl@gmail.com>
2022-10-04 23:53:12 +0000

    test: add TestParseChangeKarmaArgs

--- cmd/popplebot/internal/command/args_test.go
+++ cmd/popplebot/internal/command/args_test.go
@@ -2,7 +2,10 @@ package command
 
 import (
 	"errors"
+	"reflect"
 	"testing"
+
+	"github.com/connorkuehl/popple"
 )
 
 func TestParseSetAnnounceArgs(t *testing.T) {
@@ -52,6 +55,37 @@ func TestParseSetAnnounceArgs(t *testing.T) {
 
 			if got != tt.want.arg {
 				t.Errorf("want arg=%v, got arg=%v", tt.want.arg, got)
+			}
+		})
+	}
+}
+
+func TestParseChangeKarmaArgs(t *testing.T) {
+	tests := []struct {
+		input string
+		want  popple.Increments
+	}{
+		{
+			input: "a++ b-- (c and d)++",
+			want: popple.Increments{
+				"a":       1,
+				"b":       -1,
+				"c and d": 1,
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.input, func(t *testing.T) {
+			var got ChangeKarmaArgs
+
+			err := got.ParseArg(tt.input)
+			if err != nil {
+				t.Errorf("unexpected error: %v", err)
+			}
+
+			if !reflect.DeepEqual(tt.want, got.Increments) {
+				t.Errorf("want %v, got %v", tt.want, got)
 			}
 		})
 	}
